2e365e8676c93df96fdd0e092b14562890d02494
Renzo Aimaretti <renzoceronueve@gmail.com>
2024-08-11 20:02:20 -0300

    merge de datos del master con login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { MovieComponent } from './pages/movie/movie.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ShowMoviesComponent } from './pages/show-movies/show-movies.component.js';
 export const routes: Routes = [
   //Rutas de la aplicacion
   { path: 'users', component: UserComponent },
@@ -15,8 +14,7 @@ export const routes: Routes = [
   {path: 'users/:id', component: UserDetailsComponent},
   {path:'movie/:id',component : MovieComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'movies', component: ShowMoviesComponent}
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
