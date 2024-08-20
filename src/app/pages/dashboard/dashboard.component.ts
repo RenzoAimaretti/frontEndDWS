57c1098113be4971eba48ccf7c9f3bcca9d8be24
Renzo Aimaretti <renzoceronueve@gmail.com>
2024-08-20 15:58:05 -0300

    integracion de jwt

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../interface/user';
+import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -11,13 +12,20 @@ import { User } from '../../interface/user';
 })
 export class DashboardComponent {
   userLoginOn: boolean = false;
-  userData?: User;
-  constructor(private authService:AuthService) { }
+  user?: User
+  id?:number
+  constructor(private authService:AuthService, private userService:UserService) { }
 
   ngOnInit(): void {
     this.authService.isUserLoggedIn().subscribe({
       next: (response) => {
         this.userLoginOn = response;
+        const currentId =  Number(this.authService.currentUserId.value);
+        this.userService.getUser(currentId).subscribe({
+        next: (Response)=>{
+        this.user=Response;
+      }
+    })
       },
       error: (error) => {
         console.log(error);
@@ -26,25 +34,8 @@ export class DashboardComponent {
         console.log('Complete');
       }
     });
-    this.authService.getUserData().subscribe({
-      next: (userData) => {
-        this.userData = userData;
-      },
-      error: (error) => {
-        console.log(error);
-      },
-      complete: () => {
-        console.log('Complete');
-      }
-    });
+    
   }
+  
 
-  ngOnDestroy(): void {
-    if (this.authService.currentUserLoginOn) {
-      this.authService.currentUserLoginOn.unsubscribe();
-    }
-    if (this.authService.currentUserData) {
-      this.authService.currentUserData.unsubscribe();
-    }
-  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,29 +3,27 @@ import { LoginRequest } from '../interface/loginRequest';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, Observable,throwError, BehaviorSubject, tap, map } from 'rxjs';
 import { User } from '../interface/user';
+import { CookieService } from 'ngx-cookie-service';
 @Injectable({providedIn: 'root'})
 export class AuthService {
   authUrl = 'http://localhost:3000/api/auth/';
 
   currentUserLoginOn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  currentUserData: BehaviorSubject<User> = new BehaviorSubject<User>({id:0,
-    name:'',
-    email:'',
-    rangoCinefilo:{
-      id:0,
-      nameRango:'',
-      descriptionRango:''},
-    subscription:{
-      id:0,
-      name:'',
-      cantidadSem:0}});
-      
-  constructor(private http:HttpClient) { }
+  currentUserData: BehaviorSubject<string> = new BehaviorSubject<string>('')
+  currentUserId= new BehaviorSubject<string>('')
+  constructor(private http:HttpClient,private cookieService: CookieService) { 
+    const accessToken = this.cookieService.get('access_token');
+    this.currentUserLoginOn = new BehaviorSubject<boolean>(accessToken!=null);
+    this.currentUserData= new BehaviorSubject<string>(accessToken||'');
+    
+  }
 
   login(credentials: LoginRequest): Observable<User> {
-    return this.http.post<User>(`${this.authUrl}login`, credentials).pipe(
-      map((response: any) => response.data),
+    return this.http.post<any>(`${this.authUrl}login`, credentials).pipe(
       tap((userData) => {
+        const {data,token}=userData
+        this.currentUserId.next(data.id)
+        this.cookieService.set('access_token', token);
         this.currentUserData.next(userData);
         this.currentUserLoginOn.next(true);
       }),
@@ -33,6 +31,16 @@ export class AuthService {
     );
   }
   
+  logout(): Observable<void> {
+    return this.http.post<any>(`${this.authUrl}logout`, {}).pipe(
+      tap(() => {
+        this.cookieService.delete('access_token');
+        this.currentUserData.next('');
+        this.currentUserLoginOn.next(false);
+      }),
+      catchError(this.handleError)
+    );
+  }
 
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
@@ -45,7 +53,7 @@ export class AuthService {
     return throwError(() => new Error('Algo malo paso; por favor, intente de nuevo mas tarde.'));
   }
 
-  getUserData(): Observable<User> {
+  getUserData(): Observable<string> {
     return this.currentUserData.asObservable();
   }
 
