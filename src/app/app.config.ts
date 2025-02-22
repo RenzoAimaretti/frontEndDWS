29569d1b355c7b57618ef7f4bafdc620261c22dc
Renzo Aimaretti <renzoceronueve@gmail.com>
2025-02-21 23:36:26 -0300

    paso a.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,36 +1,36 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { UserService } from './services/user.service';
+import { UserService } from './services/user.service.js';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideProtractorTestingSupport } from '@angular/platform-browser';
-import { authInterceptor } from './custom/auth.interceptor';
+import { authInterceptor } from './custom/auth.interceptor.js';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MovieComponent } from './pages/movie/movie.component';
-import { HomeComponent } from './pages/home/home.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './pages/login/login.component';
+import { MovieComponent } from './pages/movie/movie.component.js';
+import { HomeComponent } from './pages/home/home.component.js';
+import { DashboardComponent } from './pages/dashboard/dashboard.component.js';
+import { LoginComponent } from './pages/login/login.component.js';
 import { ShowMoviesComponent } from './pages/show-movies/show-movies.component.js';
 import { ShowListsComponent } from './pages/show-lists/show-lists.component.js';
-import { authGuard } from './custom/auth.guard';
-import { RegisterComponent } from './pages/register/register.component';
+import { authGuard } from './custom/auth.guard.js';
+import { RegisterComponent } from './pages/register/register.component.js';
 import { UserEditComponent } from './pages/user-edit/user-edit.component.js';
 import { ListDetailsComponent } from './pages/list-details/list-details.component.js';
 import { ListCreateComponent } from './pages/list-create/list-create.component.js';
 import { UserListsComponent } from './pages/user-lists/user-lists.component.js';
 import { ListEditComponent } from './pages/list-edit/list-edit.component.js';
-import { ShowUsersComponent } from './pages/show-users/show-users.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
-import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
-import { CreateRangoCinefiloComponent } from './pages/create-rango-cinefilo/create-rango-cinefilo.component';
-import { CreateSubscriptionComponent } from './pages/create-subscription/create-subscription.component';
-import { ShowRangosComponent } from './pages/show-rangos/show-rangos.component';
-import { ShowSubscriptionsComponent } from './pages/show-subscriptions/show-subscriptions.component';
-import { EditarSubscripcionComponent } from './pages/editar-subscripcion/editar-subscripcion.component';
-import { EditarRangoCinefiloComponent } from './pages/editar-rango-cinefilo/editar-rango-cinefilo.component';
-import { SuggestionsManagerComponent } from './pages/suggestions-manager/suggestions-manager.component';
-import { UpgradeSubscriptionComponent } from './pages/upgrade-subscription/upgrade-subscription.component';
+import { ShowUsersComponent } from './pages/show-users/show-users.component.js';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component.js';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component.js';
+import { CreateRangoCinefiloComponent } from './pages/create-rango-cinefilo/create-rango-cinefilo.component.js';
+import { CreateSubscriptionComponent } from './pages/create-subscription/create-subscription.component.js';
+import { ShowRangosComponent } from './pages/show-rangos/show-rangos.component.js';
+import { ShowSubscriptionsComponent } from './pages/show-subscriptions/show-subscriptions.component.js';
+import { EditarSubscripcionComponent } from './pages/editar-subscripcion/editar-subscripcion.component.js';
+import { EditarRangoCinefiloComponent } from './pages/editar-rango-cinefilo/editar-rango-cinefilo.component.js';
+import { SuggestionsManagerComponent } from './pages/suggestions-manager/suggestions-manager.component.js';
+import { UpgradeSubscriptionComponent } from './pages/upgrade-subscription/upgrade-subscription.component.js';
 //migracion del codigo de rutas que se encintraba en app.routes.ts. Middle issue
 const routes: Routes = [
   //Rutas de la aplicacion
