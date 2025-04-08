5bc80b9141ee37c64be6dba269634662c245ca45
Renzo Aimaretti <renzoceronueve@gmail.com>
2025-04-08 12:12:23 -0300

    small fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  domainBack: 'backend-production-e3fa.up.railway.app',
+  domainBack: 'https://backend-production-e3fa.up.railway.app',
   domainFront: 'http://localhost:4200',
   apiKeyTMBD: 'bf8165ffd8ab851cb56ba8f197507c52',
 };
